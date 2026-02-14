860a2b2770ac83226af80de4950b27865f6e5f3a
Lotar <amaral.lucas.lotar#gmail.com>
2026-02-14 18:42:02 -0300

    Completed Home Component

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -12,15 +12,19 @@ const Home = ({ headerHeight }: HomeProps) => {
       className="flex justify-center bg-[#eff0ec] w-full px-2 xs:px-3 sm:px-6"
       style={{ minHeight: `calc(100vh - ${headerHeight}px)` }}
     >
-      <div className="flex justify-around items-center max-w-[1400px] h-full pt-8 sm:pt-12 lg:pt-20 pb-6 sm:pb-9">
-        <div className="flex flex-col lg:flex-row justify-center items-center gap-6 sm:gap-8 lg:gap-12 w-full" data-aos="fade-up">
+      <div className="flex justify-around items-center max-w-350 h-full pt-8 sm:pt-12 lg:pt-20 pb-6 sm:pb-9">
+        <div
+          className="flex flex-col lg:flex-row justify-center items-center gap-6 sm:gap-8 lg:gap-12 w-full"
+          data-aos="fade-up"
+        >
           <div className="w-full lg:flex-2 text-left flex flex-col justify-between gap-3 sm:gap-4">
-            <div className="flex flex-col gap-3 sm:gap-4">
+            <div className="flex flex-col gap-3 sm:gap-2">
               <span className="text-sm sm:text-base lg:text-lg text-[#003366] font-medium">
                 Seja bem-vindo(a)!
               </span>
 
-              <h1 className="text-2xl sm:text-3xl lg:text-4xl xl:text-5xl font-serif font-bold text-[#003366] leading-tight pt-4 sm:pt-8 lg:pt-12">
+              <h1 className="text-2xl sm:text-3xl lg:text-4xl xl:text-5xl font-serif font-bold text-[#003366]
+              leading-tight pt-4 sm:pt-8 lg:pt-12">
                 Um espaço de acolhimento para cuidar da sua saúde mental
               </h1>
 
@@ -32,53 +36,63 @@ const Home = ({ headerHeight }: HomeProps) => {
               </p>
             </div>
 
-            {/* Botões Desktop - aparecem só em lg */}
+            {/* Botões Desktop */}
             <div className="hidden lg:flex flex-col sm:flex-row gap-3 sm:gap-4 pt-4 sm:pt-6 lg:pt-8">
-              <button className="bg-yellow-400 text-white font-semibold px-4 py-2 sm:px-6 sm:py-3 rounded-full hover:bg-yellow-500 transition-colors">
+              <button className="bg-yellow-400 text-white font-semibold px-4 py-2 sm:px-6 sm:py-3
+                rounded-full hover:bg-yellow-500 transition-colors">
+
                 <span className='text-sm sm:text-base lg:text-lg'>Saiba mais sobre mim</span>
               </button>
 
-              <button className="bg-yellow-400 text-white font-semibold px-4 py-2 sm:px-6 sm:py-3 rounded-full hover:bg-yellow-500 transition-colors">
+              <button className="bg-yellow-400 text-white font-semibold px-4 py-2 sm:px-6 sm:py-3 
+                rounded-full hover:bg-yellow-500 transition-colors">
+
                 <span className='text-sm sm:text-base lg:text-lg'>Entre em contato</span>
               </button>
             </div>
           </div>
 
           <div className="w-full lg:flex-2 flex items-center justify-center">
-              <div className="grid grid-cols-2 gap-4 sm:gap-6 lg:gap-12 w-full pt-8 sm:pt-16 lg:pt-28">
-                <div className="bg-[#9db6cc] rounded-2xl sm:rounded-3xl p-3 sm:p-4 lg:p-6 flex flex-col items-center justify-center text-center min-h-[100px] sm:min-h-[120px] lg:min-h-25">
-                  <Heart className="w-8 h-8 sm:w-10 sm:h-10 lg:w-13 lg:h-13 text-yellow-400" />
+              <div className="grid grid-cols-2 gap-3 sm:gap-4 lg:gap-6 w-full pt-8 sm:pt-16 lg:pt-28">
+                <div className="bg-[#9db6cc] rounded-2xl sm:rounded-3xl p-3 sm:p-3 lg:p-4
+                flex flex-col items-center justify-center text-center min-h-20 sm:min-h-24 lg:min-h-20">
+                  <Heart className="w-8 h-8 sm:w-10 sm:h-10 lg:w-12 lg:h-12 text-yellow-400" />
 
-                  <h3 className="text-xs sm:text-sm lg:text-base font-bold text-blue-900 mt-2">Acolhimento</h3>
+                  <h3 className="text-base sm:text-lg lg:text-xl font-bold text-blue-900 mt-2">Acolhimento</h3>
                 </div>
 
-                <div className="bg-[#9db6cc] rounded-2xl sm:rounded-3xl p-3 sm:p-4 lg:p-6 flex flex-col items-center justify-center text-center min-h-[100px] sm:min-h-[120px] lg:min-h-25">
-                  <Lock className="w-8 h-8 sm:w-10 sm:h-10 lg:w-13 lg:h-13 text-[#7b9f6b]" />
+                <div className="bg-[#9db6cc] rounded-2xl sm:rounded-3xl p-3 sm:p-3 lg:p-4
+                flex flex-col items-center justify-center text-center min-h-20 sm:min-h-24 lg:min-h-20">
+                  <Lock className="w-8 h-8 sm:w-10 sm:h-10 lg:w-12 lg:h-12 text-[#7b9f6b]" />
 
-                  <h3 className="text-xs sm:text-sm lg:text-base font-bold text-blue-900 mt-2">Sigilo</h3>
+                  <h3 className="text-base sm:text-lg lg:text-xl font-bold text-blue-900 mt-2">Sigilo</h3>
                 </div>
 
-                <div className="bg-[#9db6cc] rounded-2xl sm:rounded-3xl p-3 sm:p-4 lg:p-6 flex flex-col items-center justify-center text-center min-h-[100px] sm:min-h-[120px] lg:min-h-25">
-                  <FaCheckCircle className="w-8 h-8 sm:w-10 sm:h-10 lg:w-13 lg:h-13 text-white" />
+                <div className="bg-[#9db6cc] rounded-2xl sm:rounded-3xl p-3 sm:p-3 lg:p-4
+                flex flex-col items-center justify-center text-center min-h-20 sm:min-h-24 lg:min-h-20">
+                  <FaCheckCircle className="w-8 h-8 sm:w-10 sm:h-10 lg:w-12 lg:h-12 text-white" />
 
-                  <h3 className="text-xs sm:text-sm lg:text-base font-bold text-blue-900 mt-2">Escuta Qualificada</h3>
+                  <h3 className="text-base sm:text-lg lg:text-xl font-bold text-blue-900 mt-2">Escuta Qualificada</h3>
                 </div>
 
-                <div className="bg-[#9db6cc] rounded-2xl sm:rounded-3xl p-3 sm:p-4 lg:p-6 flex flex-col items-center justify-center text-center min-h-[100px] sm:min-h-[120px] lg:min-h-25">
-                  <FaHandshake className="w-8 h-8 sm:w-10 sm:h-10 lg:w-13 lg:h-13 text-[#003366]" />
+                <div className="bg-[#9db6cc] rounded-2xl sm:rounded-3xl p-3 sm:p-3 lg:p-4
+                flex flex-col items-center justify-center text-center min-h-20 sm:min-h-24 lg:min-h-20">
+                  <FaHandshake className="w-8 h-8 sm:w-10 sm:h-10 lg:w-12 lg:h-12 text-[#003366]" />
 
-                  <h3 className="text-xs sm:text-sm lg:text-base font-bold text-blue-900 mt-2">Respeito</h3>
+                  <h3 className="text-base sm:text-lg lg:text-xl font-bold text-blue-900 mt-2">Respeito</h3>
                 </div>
               </div>
           </div>
 
-          {/* Botões Mobile - aparecem só abaixo do grid em telas pequenas */}
-          <div className="w-full flex lg:hidden flex-col sm:flex-row gap-3 sm:gap-4 pt-4 sm:pt-6">
-            <button className="bg-yellow-400 text-white font-semibold px-4 py-2 sm:px-6 sm:py-3 rounded-full hover:bg-yellow-500 transition-colors">
+          {/* Botões Mobile */}
+          <div className="flex lg:hidden flex-col sm:flex-row gap-3 sm:gap-4 pt-4 sm:pt-6">
+            <button className="bg-yellow-400 text-white font-semibold px-4 py-2 sm:px-6 sm:py-3 rounded-full
+            hover:bg-yellow-500 transition-colors">
               <span className='text-sm sm:text-base'>Saiba mais sobre mim</span>
             </button>
 
-            <button className="bg-yellow-400 text-white font-semibold px-4 py-2 sm:px-6 sm:py-3 rounded-full hover:bg-yellow-500 transition-colors">
+            <button className="bg-yellow-400 text-white font-semibold px-4 py-2 sm:px-6 sm:py-3 rounded-full
+            hover:bg-yellow-500 transition-colors">
               <span className='text-sm sm:text-base'>Entre em contato</span>
             </button>
           </div>
